11a3d65af78a109d24b5f5ed86e1a69420ba003b
Sebastian-Sinclair <bash.tech.email@gmail.com>
2024-10-24 16:25:18 -0400

    Exercise 4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,23 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.reduce(
+    (sum, blog) => sum + blog.likes, 
+    0
+  )
+}
+
+const favoriteBlog = (blogs) => {
+  return blogs.find(
+    blog => blog.likes === Math.max(
+      ...blogs.map(blog => blog.likes)
+    )
+  )
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
+  favoriteBlog
 }
