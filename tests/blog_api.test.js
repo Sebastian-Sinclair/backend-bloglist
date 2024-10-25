c5e4cbf86ed98f25b1b5e4daa729ef48e8d9141c
Sebastian-Sinclair <bash.tech.email@gmail.com>
2024-10-24 21:26:03 -0400

    Added more testing and changed routes to async functions

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -2,56 +2,37 @@ const { test, after, beforeEach } = require('node:test')
 const assert = require('node:assert')
 const Blog = require('../models/blog')
 const mongoose = require('mongoose')
+const helper = require('./test_helper')
 const supertest = require('supertest')
 const app = require('../app')
 
 const api = supertest(app)
 
-const initialBlogs = [
-  {
-    _id: "5a422a851b54a676234d17f7",
-    title: "React patterns",
-    author: "Michael Chan",
-    url: "https://reactpatterns.com/",
-    likes: 7,
-    __v: 0
-  },
-  {
-    _id: "5a422aa71b54a676234d17f8",
-    title: "Go To Statement Considered Harmful",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-    likes: 5,
-    __v: 0
-  }
-]
-
 beforeEach(async () => {
   await Blog.deleteMany({})
-  let blogObject = new Blog(initialBlogs[0])
-  await blogObject.save()
-  blogObject = new Blog(initialBlogs[1])
-  await blogObject.save()
+
+  const blogObjects = helper.initialBlogs
+    .map(blog => new Blog(blog))
+  
+  const promiseArray = blogObjects.map(blog => blog.save())
+  await Promise.all(promiseArray)
 })
 
-test('blogs are returned as json', async () => {
+test('blogs are returned as json and the correct amount of blogs', async () => {
   await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
+
+  const blogs = await helper.blogsInDb()
+  
+  assert.strictEqual(helper.initialBlogs.length, blogs.length)
 })
 
 test('there are two blogs', async () => {
   const response = await api.get('/api/blogs')
 
   assert.strictEqual(response.body.length, 2)
-})
-
-test('the first blog is about react', async () => {
-  const response = await api.get('/api/blogs')
-
-  const contents = response.body.map(e => e.title)
-  assert(contents.includes('React patterns'))
 })
 
 test('a valid blog can be added', async () => {
@@ -68,12 +49,10 @@ test('a valid blog can be added', async () => {
     .expect(201)
     .expect('Content-Type', /application\/json/)
   
-  const response = await api.get('/api/blogs')
+  const blogs = await helper.blogsInDb()
+  assert.strictEqual(blogs.length, helper.initialBlogs.length + 1)
 
-  const titles = response.body.map(r => r.title)
-
-  assert.strictEqual(response.body.length, initialBlogs.length + 1)
-
+  const titles = blogs.map(r => r.title)
   assert(titles.includes("hello world"))
 })
 
@@ -90,11 +69,40 @@ test('blog without likes stated defaults to 0', async () => {
     .expect(201)
     .expect('Content-Type', /application\/json/)
   
-  const response = await api.get('/api/blogs')
+    const blogs = await helper.blogsInDb()
 
-  const blog = response.body.find(blog => blog.title === "tech is fun")
+  const blog = blogs.find(blog => blog.title === "tech is fun")
 
   assert.strictEqual(blog.likes, 0)
+})
+
+test('a specific blog can be viewed', async () => {
+  const blogsAtStart = await helper.blogsInDb()
+
+  const blogToView = blogsAtStart[0]
+
+  const resultBlog = await api
+    .get(`/api/blogs/${blogToView.id}`)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  assert.strictEqual(JSON.stringify(resultBlog.body.toString()), JSON.stringify(blogToView.toString()))
+})
+
+test('a blog can be deleted', async () => {
+  const blogsAtStart = await helper.blogsInDb()
+  const blogToDelete = blogsAtStart[0]
+
+  await api
+    .delete(`/api/blogs/${blogToDelete.id}`)
+    .expect(204)
+
+  const blogsAtEnd = await helper.blogsInDb()
+
+  const titles = blogsAtEnd.map(r => r.titles)
+  assert(!titles.includes(blogToDelete.title))
+
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1)
 })
 
 after(async () => {
---
+++ tests/test_helper.js
@@ -0,0 +1,29 @@
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+  {
+    _id: "5a422bc61b54a676234d17fc",
+    title: "Type wars",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+    likes: 2,
+    __v: 0
+  },
+  {
+    _id: "671ab6b5aeac454d5b1f380a",
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    likes: 12,
+    __v: 0
+  }
+]
+
+const blogsInDb = async () => {
+  const blogs = await Blog.find({})
+  return blogs.map(blog => blog.toJSON())
+}
+
+module.exports = {
+  initialBlogs, blogsInDb
+}
